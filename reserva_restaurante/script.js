1640e6d0a1eaf3dd97252b2bd471eef49f691a39
Caic Rangel <caicrangel@gmail.com>
2023-11-21 11:57:31 -0300

    Modificando e adicionando elementos no script

--- reserva_restaurante/script.js
+++ reserva_restaurante/script.js
@@ -44,12 +44,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Simulação de dados fictícios de restaurantes
   const restaurantes = [
-    { id:'0001', nome: 'Restaurante 1', descricao: 'Comida deliciosa', foto: './img-restaurantes/rest1.jpg', localizacao: 'Rua 1' },
-    { id:'0002', nome: 'Restaurante 2', descricao: 'Ótima vista', foto: './img-restaurantes/rest2.jpg', localizacao: 'Rua 2' },
-    { id:'0003', nome: 'Restaurante 3', descricao: 'Cozinha internacional', foto: './img-restaurantes/rest3.jpg', localizacao: 'Rua 3' },
-    { id:'0004', nome: 'Restaurante 3', descricao: 'Cozinha internacional', foto: './img-restaurantes/rest4.jpg', localizacao: 'Rua 3' },
-    { id:'0005', nome: 'Restaurante 3', descricao: 'Cozinha internacional', foto: './img-restaurantes/rest5.jpg', localizacao: 'Rua 3' },
-    { id:'0006', nome: 'Restaurante 3', descricao: 'Cozinha internacional', foto: './img-restaurantes/rest6.jpg', localizacao: 'Rua 3' }
+    { id:'0001', nome: 'Restaurante 1', descricao: 'Comida deliciosa', foto: './img-restaurantes/rest1.jpg', localizacao: 'Rua 1', valor: 70 },
+    { id:'0002', nome: 'Restaurante 2', descricao: 'Ótima vista', foto: './img-restaurantes/rest2.jpg', localizacao: 'Rua 2', valor: 40 },
+    { id:'0003', nome: 'Restaurante 3', descricao: 'Cozinha internacional', foto: './img-restaurantes/rest3.jpg', localizacao: 'Rua 3', valor: 80 },
+    { id:'0004', nome: 'Restaurante 3', descricao: 'Cozinha internacional', foto: './img-restaurantes/rest4.jpg', localizacao: 'Rua 3', valor: 100 },
+    { id:'0005', nome: 'Restaurante 3', descricao: 'Cozinha internacional', foto: './img-restaurantes/rest5.jpg', localizacao: 'Rua 3', valor: 50 },
+    { id:'0006', nome: 'Restaurante 3', descricao: 'Cozinha internacional', foto: './img-restaurantes/rest6.jpg', localizacao: 'Rua 3', valor: 60 }
 ];
 
 
@@ -69,25 +69,107 @@ document.addEventListener('DOMContentLoaded', function () {
       restaurantList.appendChild(listItem);
   });
 
- 
+  function resetValoresCarrinho() {
+    quantidadeSubtotal.innerText = '0 Pessoa';
+    valorSubtotal.innerText = '0.00';
+    contagem = 0;
+  }
 
-  // // Adicione um evento de envio para o formulário de reserva
-  // const reservationForm = document.getElementById('reservation-form');
-  // reservationForm.addEventListener('submit', function (event) {
-  //     event.preventDefault();
-  //     // Lógica de reserva aqui (enviar para o servidor, etc.)
-  //     closeModal();
-  // });
+  //Incrementando a capa do restaurante em cada botão
+  const reservarButtons = document.querySelectorAll('.reservar-btn');
+  let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
+  let valorSubtotal = document.getElementById("valor-subtotal");
 
-  // // Adicione um evento de clique para abrir o modal quando o botão "Reservar" é clicado
-  // restaurantList.addEventListener('click', function (event) {
-  //     const target = event.target;
-  //     if (target.classList.contains('reservar-btn')) {
-  //         openModal();
-  //     }
-  // });
+  reservarButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      resetValoresCarrinho()
+      const restauranteId = button.getAttribute('id');
+      const restaurante = restaurantes.find(r => r.id === restauranteId);
 
+      if (restaurante) {
+        const itemCarrinho = document.querySelector('.item-carrinho');
+        itemCarrinho.innerHTML = `
+        <div class="capa">
+          <img id="restArray" src="${restaurante.foto}" alt="${restaurante.descricao}">
+        </div>
+        <h3>${restaurante.nome}</h3>
+        <h4>${restaurante.descricao}</h4>
+        <p>R$ ${restaurante.valor} Pessoa</p>
+        <button id="btn-subtrair-produto-01">-</button>
+        <input class="inputCarrinho" id="quantidade-produto-01" type="number" value="1" min="0">
+        <button id="btn-adicionar-produto-01">+</button>
+        <button id="btn-excluir-produto-01">EXCLUIR</button>
+        <label for="data-selecionada">Data:</label>
+        <input type="date" id="data-selecionada" name="data-selecionada">
+        `
+
+        let botaoAdd = document.querySelector('#btn-adicionar-produto-01')
+        let botaoSubtrair = document.querySelector('#btn-subtrair-produto-01')
+        let qtdProdutos = document.querySelector('#quantidade-produto-01')
+        qtdProdutos.value = 0
+        contagem = 0
+        
+        botaoAdd.addEventListener('click',() => {
+          contagem = contagem +1
+          qtdProdutos.value = contagem
+          atualizarQuantidadeSubtotal();
+        })
+        
+        botaoSubtrair.addEventListener('click',() => {
+          if(contagem > 0){
+            contagem = contagem -1
+          qtdProdutos.value = contagem
+          atualizarQuantidadeSubtotal();
+          }
+        })
+        
+        let excluirProdutos = document.querySelector('#btn-excluir-produto-01')
+        
+        excluirProdutos.addEventListener('click',() =>{
+          contagem = 0
+          qtdProdutos.value = contagem
+          atualizarQuantidadeSubtotal();
+        })
+        
+        qtdProdutos.addEventListener('blur',() =>{
+          contagem = qtdProdutos.value
+          atualizarQuantidadeSubtotal();
+        
+        })
+        
+
+        
+        function atualizarQuantidadeSubtotal() {
+        const inputItens = qtdProdutos.value > 1 ? ' Pessoas' : ' Pessoa'
+        quantidadeSubtotal.innerText = qtdProdutos.value + inputItens
+        let calc = qtdProdutos.value * restaurante.valor
+        valorSubtotal.innerText = calc.toFixed(2);
+        }
+
+        const btnreservar = document.getElementById('btn-reservar')
+        const dataSelecionadaInput = document.getElementById('data-selecionada')
+
+        btnreservar.addEventListener('click',()=>{
+         
+          const dataSelecionada = dataSelecionadaInput.value
+          const valorTotal = valorSubtotal.innerText
+
+          closeModal()
+          mostrarCaixa();
+
+          
+        const resumo = document.getElementById('resumo')
+        resumo.innerHTML = `
+          <h3>${restaurante.nome}</h3>
+          <h3>Data Selecionada: ${dataSelecionada}</h3>
+          <h3>Valor Total: R$ ${valorTotal}</h3>
+        `
+        })
+      }
+    });
+  });
 });
+
 
 function openModal() {
   const reservationModal = document.getElementById('reservation-modal');
@@ -100,75 +182,14 @@ function closeModal() {
 }
 
 
+// Manipulando a caixa de reserva concluida
 
-//Script do carrinho de compras
+function mostrarCaixa() {
+  const caixa = document.getElementById('reservaConcluidaCaixa');
+  caixa.style.display = 'block';
+}
 
-
-// //percorrendo array restaurantes
-// const btn = document.querySelector('.reservar-btn');
-
-// for (let i = 0; i < btn.length; i++) { 
-//   console.log(btn[i].id);  
-
-//   // if (restaurantes[i].id === '0001') {
-    
-//   //   capaRest.innerHTML = `
-//   //     <img id="restArray" src="${restaurantes[i].foto}" alt="${restaurantes[i].descricao}">
-//   //     `
-//   //   break;
-
-//   // }
-// }
-
-
-
-
-let subtotalInfo = {
-    quantidade: 1,
-    valor: 11.66,
-  };
-  let botaoAdd = document.querySelector('#btn-adicionar-produto-01')
-  let botaoSubtrair = document.querySelector('#btn-subtrair-produto-01')
-  let qtdProdutos = document.querySelector('#quantidade-produto-01')
-  qtdProdutos.value = 0
-  contagem = 0
-  
-  botaoAdd.addEventListener('click',() => {
-    contagem = contagem +1
-    qtdProdutos.value = contagem
-    atualizarQuantidadeSubtotal();
-  })
-  
-  botaoSubtrair.addEventListener('click',() => {
-    if(contagem > 0){
-      contagem = contagem -1
-    qtdProdutos.value = contagem
-    atualizarQuantidadeSubtotal();
-    }
-  })
-  
-  let excluirProdutos = document.querySelector('#btn-excluir-produto-01')
-  
-  excluirProdutos.addEventListener('click',() =>{
-    contagem = 0
-    qtdProdutos.value = contagem
-    atualizarQuantidadeSubtotal();
-  })
-  
-  qtdProdutos.addEventListener('blur',() =>{
-    contagem = qtdProdutos.value
-    atualizarQuantidadeSubtotal();
-  
-  })
-  
-  let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
-  let valorSubtotal = document.getElementById("valor-subtotal");
-  
-  function atualizarQuantidadeSubtotal() {
-  const inputItens = qtdProdutos.value > 1 ? ' Itens' : ' Item'
-  quantidadeSubtotal.innerText = qtdProdutos.value + inputItens;
-  let calc = qtdProdutos.value * subtotalInfo.valor
-  valorSubtotal.innerText = calc.toFixed(2);
-  }
-  
-  
+function fecharCaixa() {
+  const caixa = document.getElementById('reservaConcluidaCaixa');
+  caixa.style.display = 'none';
+}
